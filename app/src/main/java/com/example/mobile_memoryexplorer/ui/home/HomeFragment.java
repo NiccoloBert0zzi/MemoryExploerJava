980b3627aecd3440b501e8ced0ac23b1bd81d892
NiccoloBertozzi <niccolo.bertozzi2@studio.unibo.it>
2024-02-23 12:21:25 +0100

    Home filter

--- app/src/main/java/com/example/mobile_memoryexplorer/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/mobile_memoryexplorer/ui/home/HomeFragment.java
@@ -1,16 +1,22 @@
 package com.example.mobile_memoryexplorer.ui.home;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 
+import com.example.mobile_memoryexplorer.R;
 import com.example.mobile_memoryexplorer.ui.addMemory.Memory;
 import com.example.mobile_memoryexplorer.MySharedData;
 import com.example.mobile_memoryexplorer.databinding.FragmentHomeBinding;
@@ -20,8 +26,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class HomeFragment extends Fragment {
 
@@ -30,15 +40,27 @@ public class HomeFragment extends Fragment {
   String email;
   private DatabaseReference dbRef;
   MySharedData mySharedData;
+  Map<String, Integer> locations = new HashMap<>();
+  List<String> filter = new ArrayList<>();
+  ArrayAdapter<String> adapterItem;
+  String filter_chosen="Mondo";
 
   public View onCreateView(@NonNull LayoutInflater inflater,
                            ViewGroup container, Bundle savedInstanceState) {
     binding = FragmentHomeBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
+    //start progress bar
     mySharedData = new MySharedData(this.getContext());
     email = MySharedData.getEmail();
     dbRef = FirebaseDatabase.getInstance().getReference("memories");
+    filter.add("Mondo");
     prepareItemData(this.getContext());
+
+    binding.autoCompleteTextView.setOnItemClickListener((parent, view, position, id) -> {
+      locations.clear();
+      filter_chosen = parent.getItemAtPosition(position).toString();
+      prepareItemData(this.getContext());
+    });
     return root;
   }
 
@@ -52,12 +74,23 @@ public class HomeFragment extends Fragment {
     dbRef.addValueEventListener(new ValueEventListener() {
       @Override
       public void onDataChange(@NonNull DataSnapshot snapshot) {
+        binding.progressBar.setVisibility(RelativeLayout.VISIBLE);
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
         list.clear();
         for (DataSnapshot memorySnapshot : snapshot.getChildren()) {
           Memory m = memorySnapshot.getValue(Memory.class);
-          if (!m.getCreator().equals(email))
-            list.add(m);
+          if (!m.getCreator().equals(email)){
+            if(filter_chosen != "Mondo"){
+                if(getlocation(m.getLatitude(), m.getLongitude()).equals(filter_chosen)){
+                  list.add(m);
+                }
+            } else {
+              list.add(m);
+            }
+          }
         }
+        if(filter.size()==1) downloadData();
         if (list.isEmpty()) {
           Toast.makeText(ctx, "No memories found", Toast.LENGTH_SHORT).show();
         } else {
@@ -68,6 +101,8 @@ public class HomeFragment extends Fragment {
             binding.recyclerView.setAdapter(new MemoriesListAdapter(list, ctx,email,false));
           }
         }
+        binding.progressBar.setVisibility(View.GONE);
+        getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
       }
 
       @Override
@@ -78,5 +113,38 @@ public class HomeFragment extends Fragment {
       }
     });
 
+  }
+  private void downloadData() {
+    for (Memory m : list) {
+      String key = getlocation(m.getLatitude(), m.getLongitude());
+      if (key == null)
+        continue;
+      if (!filter.contains(key)) {
+        filter.add(key);
+      }
+      if (!filter_chosen.isEmpty()) {
+        if (locations.containsKey(key)) {
+          locations.put(key, locations.get(key) + 1);
+        } else {
+          locations.put(key, 1);
+        }
+      }
+      prepareItemData(getContext());
+    }
+    adapterItem = new ArrayAdapter<>(getContext(), R.layout.filter_list_item, filter);
+    binding.autoCompleteTextView.setAdapter(adapterItem);
+  }
+  private String getlocation(String lat, String lon) {
+    Geocoder gcd = new Geocoder(getContext(), Locale.getDefault());
+    List<Address> addresses;
+    try {
+      addresses = gcd.getFromLocation(Double.parseDouble(lat), Double.parseDouble(lon), 1);
+      if (addresses.size() > 0) {
+        return addresses.get(0).getCountryName();
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return null;
   }
 }
