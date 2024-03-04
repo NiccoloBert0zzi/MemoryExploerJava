382bdbf47e799f773100eb1c2f66a93b0d03e9a7
Alex Tamagnini <alex.tamagnini@studio.unibo.it>
2024-03-04 12:04:11 +0100

    fixed memory likes

--- app/src/main/java/com/example/mobile_memoryexplorer/Database/Favourite.java
+++ app/src/main/java/com/example/mobile_memoryexplorer/Database/Favourite.java
@@ -7,32 +7,32 @@ import androidx.room.PrimaryKey;
 
 import java.io.Serializable;
 
-@Entity(tableName = "favourite")
+@Entity(tableName = "favourite", primaryKeys = {"email", "memory_id"})
 public class Favourite implements Serializable {
-  @PrimaryKey
-  @NonNull
-  private String email;
-  @ColumnInfo(name = "memory_id")
-  private String memoryId;
+    @NonNull
+    private String email;
+    @NonNull
+    @ColumnInfo(name = "memory_id")
+    private String memoryId;
 
-  public Favourite(String email, String memoryId) {
-    this.email = email;
-    this.memoryId = memoryId;
-  }
+    public Favourite(String email, String memoryId) {
+        this.email = email;
+        this.memoryId = memoryId;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public String getMemoryId() {
-    return memoryId;
-  }
+    public String getMemoryId() {
+        return memoryId;
+    }
 
-  public void setMemoryId(String memoryId) {
-    this.memoryId = memoryId;
-  }
+    public void setMemoryId(String memoryId) {
+        this.memoryId = memoryId;
+    }
 }
